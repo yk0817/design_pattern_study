dcb3c9a0aec9d05fbdeb5f962615920092985cb3
山本憲 <em072029@gmail.com>
2017-07-23 14:20:23 +0900

    no message

--- ch01/abstract_f.py
+++ ch01/abstract_f.py
@@ -3,6 +3,10 @@ import sys
 import tempfile
 import numpy
 
+def main():
+    MathFactory
+
+
 # tempfileをファイルに書き込み
 # overrideを作成
 # 消費税との掛け算を考えるか〜
@@ -11,11 +15,9 @@ class MathFactory:
     def make_file(Class,file_type):
         return Class.File(file_type)
     
-    def make_calc(arg):
-        return Class.Calc
+    def make_calc(*arg):
+        return Class.Calc(*args)
     
-
-
 
 
 if __name__ == '__main__':
