d6458c8f71b03dc013476f5f56e086bfb3dbc413
山本憲 <em072029@gmail.com>
2017-08-02 01:13:25 +0900

    写経途中

--- ch01/gameboard1.py
+++ ch01/gameboard1.py
@@ -1,1 +1,46 @@
 import io
+import os
+import sys
+import tempfile
+
+BLACK,WHITE = ("BLACK","WHITE")
+
+def main():
+    checkers = CheckersBoard()
+    print(checkers)
+    
+    chess = ChessBoard()
+    print(chess)
+    
+    if sys.platform("win"):
+        filename = os.path.join(tempfile.gettempdir(),"gameboard.txt")
+        with  open(filename,"w",encoding="utf-8") as file:
+            file.write(sys.stdout.getvalue())
+        print("wrote '{}'".format(filename),file=sys.__stdout__)
+        
+if sys.platform.startwith("win"):
+    def console(char,background):
+        return char or " "
+    sys.stdout = io.StringIO()
+else:
+    def console(char,background):
+        return "\x1B[{}m{}\x1B[0m".format(
+            43 if background == BLACK else 47, char os " ")
+
+class  AbstractBoard:
+    
+    def __init__(self,rows,columns):
+        self.board = [[None for _ in range(columns)] for _ in range(rows)]
+        self.populate_board()
+        
+    def populate_board(self):
+        raise NotImplementedError
+    
+    def  __str__(self):
+        squares = []
+        for y,row in enumerate(self.board):
+            for x,piece in enumerate(row):
+                square = console(piece,BLACK if (y + x) % 2 else WHITE)
+                squares.append(square)
+            squares.append("\n")
+        return "".join(squares)
