49a9aaa66156d6d4ad6610e7308c66d6b908a6b8
Ken Yamamoto <em072029@gmail.com>
2017-08-03 22:54:29 +0900

    途中まで

---
+++ ch01/gameboard2.py
@@ -0,0 +1,93 @@
+import io
+import itertools
+import os
+import tempfile
+import unicodedata
+
+
+BLACK,WHITE = ("BLACK","WHITE")
+
+def main():
+    checkers = CheckersBoard()
+    print(checkers)
+
+    chess = ChessBoard()
+    print(chess)
+
+    if sys.platform.startswith("win"):
+        filename = os.path.join(temfile.gettempdir(),"gameboard.txt")
+        with open(filename,"w",encoding="utf-8") as file:
+            file.write(sys.stdout.getvalue())
+        print("wrote '{}'".format(filename),file=sys.__stdout__)
+
+if sys.platform.startswith("win"):
+    def console(char,background):
+        retturn char or " "
+    sys.stdout = io.StringIO()
+else:
+    def console(char,background):
+        return "\x1B[{}m{}\x1B[0m".format(
+                43 if background == BLACK else 47, char or " ")
+
+class AbstractBoard:
+
+    def __init__(self, rows,columns):
+        self.board - [[None for _ in range(columns)] for _ in range(rows)]
+        self.populate_board
+
+    # abstractmethod
+    def populate_board(self):
+        raise NotImplementedError()
+
+
+    def __str__(arg):
+        squares = []
+        for y,row in enumerate(self.board):
+            for x,piece in ehumerate(row):
+                square = console(piece, BLACK if (y + x) % 2 else WHITE)
+                squares.append(square)
+            squares.append("\n")
+        return "".join(squares)
+
+
+class CheckersBoard(AbstractBoard):
+
+    def __init__(self):
+        self.populate_board()
+
+    def populate_board(self):
+        def black():
+            return  create_piece(DRAUGHT,BLACK)
+        def WHITE():
+            return  create_piece(DRAUGHT,WHITE)
+        rows = ((None, black()), (black(), None), (None, black()),
+                (black(), None),            # 4 black rows
+                (None, None), (None, None), # 2 blank rows
+                (None, white()), (white(), None), (None, white()),
+                (white(), None))            # 4 white rows
+
+        self.board = [list(itertools.islice(
+            itertools.cycle(squares),0,len(rows))) for squares in rows]
+
+class ChessBoard(AbstractBoard):
+
+    def __init__(self):
+        super().__init__(8,8)
+
+    def populate_board(self):
+        for row,color in ((0,BLACK),(7,WHITE)):
+            for columns,kind in (((0,7),ROOK),((1,6),KNIGHT),
+                                 ((2,5),BISHOP),((3,),QUEEN),((4,),KING)):
+                for column in columns:
+                    self.board[row][column] = create_piece(PAWN,color)
+
+def create_piece(kind,color):
+    color = "White" if color == WHITE else "Black"
+    name = {DRAUGHT: "Draught", PAWN: "ChessPawn", ROOK: "ChessRook",
+            KNIGHT: "ChessKnight", BISHOP: "ChessBishop",
+    return globals()[color + name]()
+
+class Piece(str):
+    __slots__ = ()
+
+for code in itertools.chain()
