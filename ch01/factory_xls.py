e3ea5134e3ac29062dde7053196b61641fede3aa
Ken Yamamoto <em072029@gmail.com>
2017-08-05 03:56:10 +0900

    rename

--- ch01/factory_xls.py
+++ ch01/factory_xls.py
@@ -6,42 +6,41 @@ import re
 from abc import ABCMeta, abstractmethod
 
 # 抽象メソッド
-class AbstExcelFactory(metaclass=ABCMeta):
+class ExcelFactory(metaclass=ABCMeta):
     # デコレータをつけると、サブクラスはオーバーライドしないとエラーになる。
     @abstractmethod
     def read_file():
         pass
 
-class XlsRead(AbstExcelFactory):
+class XlsRead(ExcelFactory):
     def __init__(self,file):
         self.__file_name = file
         print(self.__file_name + "の読み取りを行います。")
 
-    def read_file(arg):
-        pass
+    def read_file(self,worksheet):
+        books = openpyxl.load_workbook(worksheet)
 
-
-class MxlsRead(AbstExcelFactory):
+class MxlsRead(ExcelFactory):
     def __init__(self,file):
         self.__file_name = file
         print(self.__file_name + "の読み取りを行います。")
 
-    def read_file(arg):
-        pass
+    def read_file(self,worksheet):
+        books = xlrd.open_workbook(worksheet)
+        print(books.sheet_buy_index(1).cell_value(rowx=0,colx=0))
 
 def main():
     # インスタンス作成
     xls_instance = XlsRead()
     mxls_instance = MxlsRead()
+    # 実行ファイル元にエクセルファイルがあると想定
     worksheets = glob.glob('./*')
 
     for worksheet in worksheets:
         if re.match(r"\.xls$",worksheet):
-
+            xls_instance.read_file(worksheet)
         elif re.match(r"\.xlsm$",worsheet):
-
-        else:
-            pass
+            mxls_instance.read_file(worksheet)
 
 
 if __name__ == '__main__':
