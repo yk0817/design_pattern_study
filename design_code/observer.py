51148ca5190f4c9797d722abc1cfe39ff2af39db
山本憲 <em072029@gmail.com>
2017-08-18 00:43:37 +0900

    途中までcommit。

--- design_code/observer.py
+++ design_code/observer.py
@@ -1,4 +1,5 @@
 import abc
+import random
 
 class Observer(metaclass=abc.ABCMeta):
     @abc.abstractmethod
@@ -12,14 +13,41 @@ class GraphObserver(Observer):
     def update(self,generator):
     
 class NumberGenerator(metaclass=ABCMeta):
+    # observer保持
+    def __init__(self):
+        self.__observers = []
     
-    def addObserver:
+    def addObserver(observer):
+        self.__observers.append(observer)
     
     def deleteObserver:
-        pass
+        self.__observers.remove(observer)
     
     def notifyObservers:
+        for Observer in self.__observers:
+            observer.update(self)
     
+    @abc.abstractmethod
     def getNumber:
+        pass
     
-    def execute
+    @abc.abstractmethod
+    def execute:
+        pass
+    
+class RandomNumberGenerator(NumberGenerator):
+    def __init__(self):
+        NumberGenerator.__init__(self)
+        self.__number = None
+    
+    def get_number(self):
+        return self.__number
+    
+    def execute(self):
+        for i in range(20):
+            self.__number = random.randint(1,50)
+            self.notify_observers()
+
+class DigitObserver(Observer):
+
+class GraphObserver(Observer):
