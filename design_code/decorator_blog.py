df7150e1fdc835d3206f396ddc56adcee8100114
山本憲 <em072029@gmail.com>
2017-08-22 00:19:06 +0900

    Decorator

---
+++ design_code/decorator_blog.py
@@ -0,0 +1,21 @@
+class PlainNumber():
+    def __init__(self,num):
+        self._num = num
+    
+    def render(self):
+        return int(self._num)
+
+class MultiplicationNumber(PlainNumber):
+    def __init__(self,num):
+        self._number = num
+    
+    def render(self,num):
+        return self._number.render() * num
+        print(self._number.render())
+    
+if __name__ == '__main__':
+    plain_num = PlainNumber(11)
+    multiple_output = MultiplicationNumber(plain_num)
+    print("11*2=",multiple_output.render(2))
+    
+
