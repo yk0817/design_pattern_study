bdcf5894fe1440f1063553613581ead7d95f34e1
山本憲 <em072029@gmail.com>
2017-08-27 20:25:01 +0900

    Design pattern ブログ用コマンドパターン作成

--- design_code/command.py
+++ design_code/command.py
@@ -1,4 +1,6 @@
-from abc import *
+import abc
+import time
+import os
 
 # csvファイル作成
 # 行追加
@@ -7,31 +9,43 @@ from abc import *
 # メソッドを簡単に追加出来る。
 
 class Command(metaclass=abc.ABCMeta):
-    def __init__(self,file_type):
-        self.file_type = file_type
-        
     @abc.abstractmethod
     def execute():
         pass
 
 class CreateFile(Command):
-    def execute():
-        pass
+    def execute(self):
+        cmd = "touch test.csv"
+        os.system(cmd)
 
 class InsertContent(Command):
-    def execute():
-        pass
+    def execute(self):
+        cmd = "date  >>test.csv"
+        os.system(cmd)
+
         
 class ReadContent(Command):
-    def execute():
-        pass
+    def execute(self):
+        cmd = "cat test.csv"
+        os.system(cmd)
 
 class DeleteFile(Command):
-    def execute():
-        pass
+    def execute(self):
+        # print("delte after... 2sec")
+        time.sleep(2)
+        cmd = "rm -rf test.csv"
+        os.system(cmd)
+
 
 def main():
+    command_array = []
+    command_array.append(CreateFile())
+    command_array.append(InsertContent())
+    command_array.append(ReadContent())
+    command_array.append(DeleteFile())
+
+    for cmd in command_array:
+        cmd.execute()
     
-        
 if __name__ == '__main__':
     main()
